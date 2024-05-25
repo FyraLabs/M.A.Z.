b2af026d7a3c333cf4221765a406ce4932382a0e
madomado <madonuko@outlook.com>
2024-05-25 23:51:11 +0800

    feat: impl list_lockers

--- desktop/src/local.rs
+++ desktop/src/local.rs
@@ -38,11 +38,24 @@ fn determine_datadir() -> PathBuf {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Event {
-    DecryptFailure(maz_auth::AuthErr),
+    #[error("Fail to decrypt M.A.Z. locker: {0}")]
+    DecryptFailure(#[from] maz_auth::AuthErr),
+    #[error("BUG: Initialization required. (You should not be able to see this message)")]
     Init,
-    IoFailure(std::io::Error),
+    #[error("Fail to access file: {0}")]
+    IoFailure(#[from] std::io::Error),
+}
+
+pub fn list_lockers() -> Result<Vec<String>, Event> {
+    let file_to_lockername = |f: std::fs::DirEntry| {
+        (f.file_name().to_string_lossy().strip_suffix(".mazl")).map(ToString::to_string)
+    };
+    let entries = std::fs::read_dir(&*DATADIR)?.filter_map(Result::ok);
+    let files = entries.filter(|f| f.metadata().is_ok_and(|meta| meta.is_file()));
+    let lockers = files.filter_map(file_to_lockername);
+    Ok(lockers.collect())
 }
 
 pub fn read_password(name: &str) -> Option<String> {
@@ -50,8 +63,8 @@ pub fn read_password(name: &str) -> Option<String> {
     entry.get_password().ok()
 }
 
-pub fn read_offline_default_locker(password: &str) -> Result<maz_auth::Locker, Event> {
-    let path = DATADIR.join("default.mazl");
+pub fn read_offline_locker(name: &str, password: &str) -> Result<maz_auth::Locker, Event> {
+    let path = DATADIR.join(format!("{name}.mazl"));
     let f = std::fs::File::open(&path);
     let Ok(mut f) = f else {
         let e = f.unwrap_err();
